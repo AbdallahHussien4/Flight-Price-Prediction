8489363b0d31e5a53382fecc6fc3a85c6486ecbf
Kareem Mohamed <kareem.mohamed9711@gmail.com>
2022-04-27 16:54:42 +0200

    text serialization

--- mapreduce/mapper.py
+++ mapreduce/mapper.py
@@ -4,10 +4,16 @@ import numpy as np
 import pickle
 
 
-data = np.loadtxt(sys.stdin.readlines(), delimiter=",")
+data = np.genfromtxt(sys.stdin.readlines(), delimiter=",")
+#data = np.zeros((1,2))
 
 #qr = np.linalg.qr(data)
-serialized = pickle.dumps(data)
+#serialized = pickle.dumps(data)
 
-sys.stdout.buffer.write(b'K\t')
-sys.stdout.buffer.write(serialized)
+#sys.stdout.buffer.write(b'K\t')
+#sys.stdout.buffer.write(len(serialized).to_bytes(1, byteorder='big'))\
+#print(f'K\t', sep='', end='')
+
+sys.stdout.write("R\t")
+np.savetxt(sys.stdout, data, delimiter=",", newline="$")
+sys.stdout.write("\n")
--- mapreduce/reducer.py
+++ mapreduce/reducer.py
@@ -1,13 +1,34 @@
 import sys
 import pickle
+import numpy as np
+from io import StringIO
 
-# reading key
-key = sys.stdin.buffer.read(1).decode()
-# reading tab separator
-sys.stdin.buffer.read(1)
-# reading value
-value = sys.stdin.buffer.read()
 
-data = pickle.loads(value)
+for line in sys.stdin:
+    #print(line)
+    key, value = line.split('\t')
+    value = value.replace('$', '\n')
+    data = np.genfromtxt(StringIO(value), delimiter=",")
+    print(key, data)
 
-print(data)
+
+# for line in sys.stdin:
+#     key, value = sys.stdin.readline().split('\t')
+#     s = StringIO(sys.stdin.read())
+#     data = np.loadtxt(sys.stdin, delimiter=",")
+#     data = pickle.loads(line)
+# print(data)
+    
+
+    # # reading key
+    # key = sys.stdin.buffer.read(1).decode()
+    # # reading tab separator
+    # sys.stdin.buffer.read(1)
+    # # reading value
+    # value = sys.stdin.buffer.read()
+
+    # #data = pickle.loads(value)
+
+    #print(key)
+#except Exception as e:
+#    print(e)
